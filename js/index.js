93daa636f4e051ef9b36d58080661c4422069884
Paul Jacks <paul.jacks@scopevisio.com>
2017-12-05 16:42:20 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ var app = angular.module('Paul', ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: "fragments/indx.html",   
-            controller: "indexController"        
+            templateUrl: "fragments/indx.html",
+            controller: "indexController"
         })
         .when('/link', {
             templateUrl: 'fragments/main.html',
@@ -14,12 +14,56 @@ app.config(function ($routeProvider) {
         .otherwise({
             redirectTo: '/'
         });
-}); 
+});
 
 app.controller('indexController', function($scope) {
-    console.log("index");
+    UiConfigs.init();
 });
 
 app.controller('mainController', function($scope) {
     console.log("hello world");
 });
+
+
+
+
+var UiConfigs = function(){
+
+  function init(){
+    configureTabs();
+  }
+
+  function configureTabs(){
+
+    for(var i = 0; i < tabs.length; i++){
+      var child = makeTab(tabs[i].color, tabs[i].link, tabs[i].text);
+      document.getElementById("menu").appendChild(child);
+    }
+
+
+
+  }
+
+  var sequencer = 0;
+  function makeTab(color, link, text){
+      var tab = document.createElement("div");
+      tab.className = "menu-item";
+      tab.id = "menu-item"+sequencer;
+      tab.style.backgroundColor = color;
+      tab.innerText = text;
+
+      sequencer++;
+
+      return tab;
+  }
+
+
+  var tabs = [
+    { color: "#98AD92", link: "#main2",  text :"hello again"},
+    { color:"#7B9475", link: "#main", text: "hello world" }
+  ];
+
+  return {
+    init : init,
+  }
+}();
